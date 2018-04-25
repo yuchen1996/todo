3614c91d5148c06693e5bd53be23f979b1e12897
YuChen <jlsp_yuchen1996@163.com>
2018-04-25 22:49:49 +0800

    2018.04.25

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.models import import User, Group
+from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from api.models import Todo
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -10,3 +11,20 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ('url', 'name')
+
+class TodoSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
+    create_time = serializers.DateTimeField()
+    update_time = serializers.DateTimeField()
+    title = serializers.CharField(required=True)
+    content = serializers.TextField()
+    target_time = serializers.DateTimeField()
+    is_end = serializers.BooleanField(default=False)
+    def create(self, validated_data):
+        return Todo.objects.create(**validated_data)
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.content = validated_data.get('content', instance.content)
+        instance.target_time = validated_data.get('target_time', instance.target_time)
+        instance.save()
+        return instance
